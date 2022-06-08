fdb6ec34d2df272c8aedaa0eb860b66b654aba41
isotelo00 <immanuelsotelo00@gmail.com>
2022-06-08 14:41:29 -0700

    Created draft sleep timer file and class

---
+++ BetterSleep/SleepTimer.swift
@@ -0,0 +1,14 @@
+//
+//  SleepTimer.swift
+//  BetterSleep
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import Foundation
+
+class SleepTimer()
+{
+    var timeSlept : Double
+    var timeGoal : Double
+}
