2bb2d5395c8b06b165b4ba35f240490a6c37a9c1
Daniel N <dnazeri@csu.fullerton.edu>
2022-06-08 14:16:37 -0700

    Initial Commit

---
+++ Shared/BetterSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleepApp.swift
+//  Shared
+//
+//  Created by Nazeri, Daniel on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
