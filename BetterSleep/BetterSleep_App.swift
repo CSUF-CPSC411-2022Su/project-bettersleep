dde8f96daabddd8cceae64fb1a7b0c37a7d72908
Tilak Ghorashainee <tilakg05@csu.fullerton.edu>
2022-06-13 21:33:26 -0700

    Complete Check in-2 UI Frame

--- BetterSleep/BetterSleep_App.swift
+++ BetterSleep/BetterSleep_App.swift
@@ -2,16 +2,15 @@
 //  BetterSleep_App.swift
 //  BetterSleep
 //
-//  Created by Nazeri, Daniel on 6/8/22.
+//  Created by Tilak Ghorashainee on 6/8/22.
 //
-
 import SwiftUI
 
 @main
 struct BetterSleep_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           ContentView()
         }
     }
 }
--- BetterSleep/ContentView.swift
+++ BetterSleep/ContentView.swift
@@ -1,21 +1,45 @@
 //
 //  ContentView.swift
-//  BetterSleep
-//
-//  Created by Nazeri, Daniel on 6/8/22.
-//
+//  AudioRecord
+//Created by tilak Ghorashainee 6/13/2022
 
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView: View{
+    var body: some View{
+        Home()
+        // darkmode color for the home scree
+            .preferredColorScheme(.dark)
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider{
     static var previews: some View {
         ContentView()
+        
     }
+}
+struct Home: View{
+    @State var record = false
+    var body: some View{
+    NavigationView{
+            VStack{
+                Button(action:{
+                   self.record.toggle()
+                }) {
+                    ZStack{
+                       Circle()
+                        .fill(Color.red)
+                        .frame(width:50, height:50)
+                  if self.record{
+                        Circle()
+                            .stroke(Color.black, lineWidth: 5)
+                            .frame(width:70, height:70)
+                            
+                    }
+                }
+            }
+                .padding(.vertical, 25)
+        }
+         .navigationBarTitle("Sleeping Audio Record")
+      }
+   }
 }
