f6f34eb7bc7f5f4b33ef478886ef9a88e5b7780d
isotelo00 <71613477+isotelo00@users.noreply.github.com>
2022-06-22 15:21:33 -0700

    added contentview

---
+++ BetterSleep/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BetterSleep
+//
+//  Created by Nazeri, Daniel on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+        }
+            .padding()
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
