1a19cf831b5dee541f3b7e90124a0ea8f6122b38
Tilak Ghorashainee <tilakg05@csu.fullerton.edu>
2022-06-27 22:39:13 -0700

    control Project 4 check in

--- BetterSleep/BetterSleep_App.swift
+++ BetterSleep/BetterSleep_App.swift
@@ -9,9 +9,9 @@ import SwiftUI
 @main
 struct BetterSleep_App: App {
     var body: some Scene {
-        WindowGroup {
+       WindowGroup {
           ContentView()
             ViewController()
-       }
+      }
     }
 }
