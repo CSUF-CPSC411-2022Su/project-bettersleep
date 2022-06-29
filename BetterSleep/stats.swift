82c448597b1b40a5113b13254a36318d61930a77
dnazeri <dnazeri@csu.fullerton.edu>
2022-06-29 01:41:50 -0700

    SwiftUICharts scrapped, class implemented

--- BetterSleep/stats.swift
+++ BetterSleep/stats.swift
@@ -5,8 +5,16 @@
 //  Created by Nazeri, Daniel on 6/20/22.
 //
 
-//import Foundation
+import Foundation
 
 // Hard-coded number of hours slept for 7 days
 //@ObservableObject var stats = [4.7, 7.5, 6.5, 5.4, 6.3, 8.1, 5.9]
 
+class Stats: ObservableObject{
+    // Properties
+    var stats = [4.7, 7.5, 6.5, 5.4, 6.3, 8.1, 5.9]
+    
+    func returnCont() -> Int{
+        return stats.count
+    }
+}
--- BetterSleep/statsViews.swift
+++ BetterSleep/statsViews.swift
@@ -5,11 +5,14 @@
 //  Created by Nazeri, Daniel on 6/20/22.
 //
 
-//import SwiftUI
+import SwiftUI
 
 // Display stats from stats.swift
-//struct statsList: View {
-//    VStack {
-//        print(stats)
-//    }
-//}
+struct statsList: View {
+    @ObservedObject var model = Model()
+    var body: some View {
+        VStack {
+            print(Stats)
+        }
+    }
+}
