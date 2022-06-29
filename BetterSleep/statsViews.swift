b9dd50fdbe19cb4e6d50fbea51284727414f6419
isotelo00 <71613477+isotelo00@users.noreply.github.com>
2022-06-29 10:40:33 -0700

    Nuclear option

---
+++ BetterSleep/statsViews.swift
@@ -0,0 +1,15 @@
+//
+//  statsViews.swift
+//  BetterSleep
+//
+//  Created by Nazeri, Daniel on 6/20/22.
+//
+
+//import SwiftUI
+
+// Display stats from stats.swift
+//struct statsList: View {
+//    VStack {
+//        print(stats)
+//    }
+//}
